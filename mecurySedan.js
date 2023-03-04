f74db4891de7e4a2254f2fadac9f847362a9bfca
NoahKKA <noahhhhh.a@gmail.com>
2023-03-04 13:25:00 -0800

    made car and added super start

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,19 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage, passenger) {
+        super(make, model, year, color, mileage, passenger)
+        this.maximumPassengers = 5;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    super.start()
+}
