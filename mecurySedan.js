80c43cafd15a77af4bcf9cd8759c91c3640ecaf6
NoahKKA <noahhhhh.a@gmail.com>
2023-03-06 17:31:16 -0800

    fixed start method

--- mecurySedan.js
+++ mecurySedan.js
@@ -7,15 +7,17 @@ console.log(v.make)
 
 
 class Car extends Vehicle {
-    constructor(make, model, year, color, mileage, passenger) {
-        super(make, model, year, color, mileage, passenger)
+    constructor(make, model, year, color, mileage, passenger,started) {
+        super(make, model, year, color, mileage, passenger,started)
         this.maximumPassengers = 5;
         this.numberOfWheels = 4;
         this.maximumSpeed = 160;
         this.fuel = 10;
         this.scheduleService = false;
     }
-    super.start();
+    start() {
+        super.start();
+    }
 
     loadPassenger (num) {
         if (num <= this.maximumPassengers) {
@@ -26,7 +28,7 @@ class Car extends Vehicle {
         }
     }
 
-    checkService (milegae) {
+    checkService () {
         if (this.milegae > 30000) {
             this.scheduleService = true;
             console.log('Time for maintenance')
